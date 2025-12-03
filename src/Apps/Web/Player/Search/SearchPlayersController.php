c474b6eb02795b1274c90af4886a803bc10a147e
Marcos <marcos.andres.romero@gmail.com>
2025-12-03 19:14:12 -0300

    fix search players

--- src/Apps/Web/Player/Search/SearchPlayersController.php
+++ src/Apps/Web/Player/Search/SearchPlayersController.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Apps\Web\Player\Search;
 
@@ -8,15 +10,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class SearchPlayersController extends ApiController
 {
-    public function __invoke(Request $request, string $sessionId): Response
+    public function __invoke(Request $request): Response
     {
         $input = SearchPlayersRequest::fromHttp($request);
         $this->validateRequest($input);
 
-        $query = $input->toQuery($sessionId);
+        $query = $input->toQuery();
         $response = $this->queryBus->ask($query);
 
         return $this->collectionResponse($response);
     }
 }
-
--- src/Apps/Web/Player/Search/SearchPlayersRequest.php
+++ src/Apps/Web/Player/Search/SearchPlayersRequest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Apps\Web\Player\Search;
 
@@ -9,42 +11,42 @@ use Symfony\Component\Validator\Constraints as Assert;
 final readonly class SearchPlayersRequest
 {
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
+        public string $userId,
         #[Assert\Type("string")]
+        #[Assert\Uuid]
         public ?string $gameId = null,
-
         #[Assert\Type("string")]
+        #[Assert\Uuid]
         public ?string $teamId = null,
-
-        #[Assert\Type("bool")]
-        public bool $mine = false,
-
         #[Assert\Type("int")]
         #[Assert\PositiveOrZero]
         public ?int $page = null,
-
         #[Assert\Type("int")]
         #[Assert\Positive]
         public ?int $limit = null,
-    ) {}
+    ) {
+    }
 
     public static function fromHttp(Request $request): self
     {
         return new self(
+            $request->query->get("userId", ""),
             $request->query->get("gameId"),
             $request->query->get("teamId"),
-            (bool) $request->query->get("mine", false),
             $request->query->getInt("page", 1),
             $request->query->getInt("limit", 20),
         );
     }
 
-    public function toQuery(string $userId): SearchPlayersQuery
+    public function toQuery(): SearchPlayersQuery
     {
         return new SearchPlayersQuery(
-            null, // query
+            null,
             $this->gameId,
             $this->teamId,
-            $this->mine ? $userId : null,
+            $this->userId,
             $this->page,
             $this->limit,
         );
